54d47f52ad4c498d4c6ef3c2625f16c9453a5fd1
酒谷冬芽 <52824423+sakatanitouga@users.noreply.github.com>
2021-01-29 22:57:05 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$tempfile = $_FILES['file']['tmp_name'];
+$filename = './' . $_FILES['file']['name'];
+ 
+if (is_uploaded_file($tempfile)) {
+    if ( move_uploaded_file($tempfile , $filename )) {
+	echo $filename . "をアップロードしました。";
+    } else {
+        echo "ファイルをアップロードできません。";
+    }
+} else {
+    echo "ファイルが選択されていません。";
+} 
+?>
